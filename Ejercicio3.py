2268b504352d6b2fc96fe1108421ed0b7a5b6795
MauroDonis <137304072+MauroDonis@users.noreply.github.com>
2023-07-05 09:45:22 -0300

    Create Ejercicio3.py

---
+++ Ejercicio3.py
@@ -0,0 +1,11 @@
+Nota1 = float(input("Ingrese la primera nota: "))
+
+Nota2 = float(input("Ingrese la segunda nota: "))
+
+Nota3 = float(input("Ingrese la tercera nota: "))
+
+Suma_notas = Nota1 + Nota2 + Nota3
+
+Promedio = Suma_notas / 3
+
+print(Promedio)
