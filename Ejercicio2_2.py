beb9cee0ffdb4b64cd3ec1902427e4d5c1e2a12b
MauroDonis <137304072+MauroDonis@users.noreply.github.com>
2023-07-05 09:22:12 -0300

    Update Ejercicio2_2.py

---
+++ Ejercicio2_2.py
@@ -0,0 +1,9 @@
+print("Ingrese la cantidad de pesos")
+Cantidad_de_plata = float(input())
+
+print("Ingrese el precio del dolar")
+Precio_dolar = float(input())
+
+Cantidad_de_dolares = Cantidad_de_plata / Precio_dolar
+
+print("con", Cantidad_de_plata, "pesos se podrian comprar ", Cantidad_de_dolares, "de dolares")
