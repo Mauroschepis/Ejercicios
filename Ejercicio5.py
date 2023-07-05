b2592b386770d175179d690b5cc67695cdd01130
MauroDonis <137304072+MauroDonis@users.noreply.github.com>
2023-07-05 11:29:44 -0300

    Ejercicios

---
+++ Ejercicio5.py
@@ -0,0 +1,9 @@
+Partidos_ganados = int(input("Ingrese la cantidad de partidos ganados "))
+
+Partidos_perdidos = int(input("Ingrese la cantidad de partidos perdidos "))
+
+Partidos_empatados = int(input("Ingrese la cantidad de partidos empatados "))
+
+puntos = Partidos_ganados * 3 + Partidos_empatados 
+
+print(puntos)
