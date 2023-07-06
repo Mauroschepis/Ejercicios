9a9787b4b0fc6605c85203af756dac630663feff
MauroDonis <137304072+MauroDonis@users.noreply.github.com>
2023-07-06 08:40:21 -0300

    Update Ejercicio6.py

---
+++ Ejercicio6.py
@@ -0,0 +1,8 @@
+Cuenta = float(input("Ingrese la cuenta: "))
+Porcentaje_propina = float(input("Ingrese el porcentaje de: "))
+
+Propina = Cuenta * Porcentaje_propina / 100
+
+print("Se debera pagar ", Propina)
+
+print("En total se pagara", Cuenta + Propina)
