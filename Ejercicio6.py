ab5412e680fd8fb42363cb3a1469aafd34de37f0
MauroDonis <137304072+MauroDonis@users.noreply.github.com>
2023-07-06 10:09:35 -0300

    Update Ejercicio6.py

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -5,4 +5,5 @@ Propina = Cuenta * Porcentaje_propina / 100
 
 print("Se debera pagar", Propina, "de propina")
 
-print("En total se pagara", Cuenta + Propina)
+total = Cuenta + Propina
+print("En total se pagara", total)
