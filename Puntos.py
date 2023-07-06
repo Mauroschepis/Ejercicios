5f81db5afb7907000d48b32cb6377b1e24a65c9f
MauroDonis <137304072+MauroDonis@users.noreply.github.com>
2023-07-06 11:46:27 -0300

    Update Puntos.py

---
+++ Puntos.py
@@ -0,0 +1,9 @@
+AX = float(input("Introduzca la coordenada X del punto A: "))
+
+AY = float(input("Introduzca la coordenada Y del punto A: "))
+
+BX = float(input("Introduzca la coordenada X del punto B: "))
+
+BY = float(input("Introduzca la coordenada X del punto B: "))
+
+distancia = (((AX-BX))**2 + ((AY - BY))**2)**0.5
