07e8e59d82cf0e43e4a2c95b90a8ddcb4093ce5b
MauroDonis <137304147+Mauroschepis@users.noreply.github.com>
2023-08-02 08:14:35 -0300

    Create Base.py

---
+++ Base.py
@@ -0,0 +1,8 @@
+base=float(input("ingresar base: "))
+alto=float(input("ingresar la altura: "))
+
+superficie=base*alto
+perimetro=2*(base+alto)
+
+print("el calculo de la superficie es: ", superficie)
+print("el calculo del perimetro es: ", perimetro)
