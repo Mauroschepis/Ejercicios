94ddaf12689a8ff125cc6153dd5bdc117f0efc7d
MauroDonis <137304147+Mauroschepis@users.noreply.github.com>
2023-08-02 09:34:03 -0300

    a

---
+++ Aprobado.py
@@ -0,0 +1,8 @@
+print("Ingrese la nota del alumno")
+A=float(input(""))
+if A>=10:
+    print ("No se puede sacar 10 o mas")
+elif A>=7:
+    print("El alumno aprobo")
+else:
+    print ("El alumno desaprobo")
