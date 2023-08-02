cb6a50e093efa58f628ac1af79b435a79c6a198e
MauroDonis <137304147+Mauroschepis@users.noreply.github.com>
2023-08-02 08:14:31 -0300

    Create cambiodolar.py

---
+++ cambiodolar.py
@@ -0,0 +1,10 @@
+soles=float(input("ingresar cantidad de soles: "))
+
+euro=3.84
+dolar=2.28
+
+dolar=soles/dolar
+euro=soles/euro
+
+print("En ", soles ,"hay ", euro , "euros")
+print("En ", soles , "hay ", dolar , "dolares")
