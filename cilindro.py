0634b57f8e45d04230451d3751815978ae44c193
MauroDonis <137304147+Mauroschepis@users.noreply.github.com>
2023-08-02 08:14:29 -0300

    Create cilindro.py

---
+++ cilindro.py
@@ -0,0 +1,12 @@
+PI=3.1416
+
+radio=float(input("ingresar radio: "))
+alto=float(input("ingresar el alto: "))
+vol=float
+area=float
+
+vol=PI*radio**2*alto
+area=2*PI*radio*(alto+radio)
+
+print("el volumen del cilindro es: ", vol)
+print("el area del cilindro es: ", area)
