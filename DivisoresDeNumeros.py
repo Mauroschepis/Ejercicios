676082b5c87a3828eacf19b20d8ce573486b2a95
MauroDonis <137304147+Mauroschepis@users.noreply.github.com>
2023-08-10 09:08:34 -0300

    Update DivisoresDeNumeros.py

--- DivisoresDeNumeros.py
+++ DivisoresDeNumeros.py
@@ -2,10 +2,9 @@ print("introduce un numero, y para acabar uno negativo")
 N=int(input(""))
 while N>0:
     suma=0
-    i=1
-    for i in range (1,int (N/2)):
+    for i in range (1,N + 1):
         print(i)
         if N%i==0:
-            suma=suma+1
+            suma=suma+i
     suma=suma+N
     print("la suma de lsos divisores del numero es", suma)
